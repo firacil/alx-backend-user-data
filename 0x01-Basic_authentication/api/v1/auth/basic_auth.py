d5a83198ce26a0b080623135c05214d6dde53a62
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-03 22:54:23 +0000

    basic auth from data

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -66,18 +66,18 @@ class BasicAuth(Auth):
         """returns the User instance based on his email and
         password
         """
-        if user_email is None or not isinstance(user_email, str):
+        if not isinstance(user_email, str) or user_email is None:
             return None
-        if user_pwd is None or not isinstance(user_pwd, str):
+        if not isinstance(user_pwd, str) or user_pwd is None:
             return None
 
         users = User.search({"email": user_email})
 
         if not users:
             return None
-        else:
-            user = users[0]
-            if not user.is_valid_password(user_pwd):
-                return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
 
         return user
