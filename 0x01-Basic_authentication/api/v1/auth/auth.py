090735d49b1b6d8d639da1664dc41d2be5b8a540
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-03 10:15:44 +0000

    require_auth updated

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,21 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """return False"""
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == "":
+            return True
+
+        if path.endswith('/'):
+            if path in excluded_paths:
+                return False
+            return True
+        else:
+            path = path + '/'
+            if path in excluded_paths:
+                return False
+            return True
 
     def authorization_header(self, request=None) -> str:
         """returns None
