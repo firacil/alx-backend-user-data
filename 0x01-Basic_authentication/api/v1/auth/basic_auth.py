2f5672660d631c3cd8020ed9cdef6b13605e33c0
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-03 21:42:32 +0000

    decoded success base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """class for basic auth"""
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,23 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[len('Basic '):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """returns the decoded value of base64string
+        """
+        if base64_authorization_header is None:
+            return None
+
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # attempt to decode the base64 string
+            byte_str = base64.b64decode(base64_authorization_header)
+            my_str = byte_str.decode('utf-8')
+        except(TypeError, ValueError):
+            # if decodeing fails, return None
+            return None
+
+        return my_str
