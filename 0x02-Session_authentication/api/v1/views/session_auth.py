1d2486161e89f6c80f75acb7ea510c528c9a5f1c
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-07 22:32:43 +0000

    session auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -2,6 +2,7 @@
 """module for view of session auth"""
 from api.v1.views import app_views
 from flask import jsonify, request, abort, make_response
+from api.v1.app import auth
 from models.user import User
 from os import getenv
 
@@ -42,3 +43,13 @@ def auth_session_login() -> str:
     response.set_cookie(session_name, session_id)
 
     return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def auth_session_logout() -> str:
+    """DELETE /auth_session/logout: handle session logout"""
+    if not auth.destroy_session(request):
+        abort(404)
+
+    return jsonify({})
