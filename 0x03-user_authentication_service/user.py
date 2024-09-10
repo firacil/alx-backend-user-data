37894a18eac4be9f81729d8983942c8c13363f0e
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-10 09:54:53 +0000

    user authenthication serice

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 """module to declare sqlalchemy model named user"""
 
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
-db = SQLAlchemy()
-
-
-class User(db.Model):
+class User(Base):
     """class User model of db called users"""
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    hashed_password = db.Column(db.String(250), nullable=False)
-    session_id = db.Column(db.String(250), nullable=True)
-    reset_token = db.Column(db.String(250), nullable=True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
