30225a3a1baa3af8a2b27fa2b00b9d9e46f51003
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-12 15:30:11 +0000

    db updated

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -37,3 +39,13 @@ class DB:
         self._session.commit()
 
         return (new_user)
+
+    def find_user_by(self, **kwargs) -> User:
+        """returns the first founded user"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
