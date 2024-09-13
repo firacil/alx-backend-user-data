077ca4b6554eccfd68b78d6aa7c193123a7b446f
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-13 18:38:41 +0000

    session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,13 @@ class Auth:
 
         # if user exist check if password is correct
         return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """_summary_"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+        else:
+            user.session_id = _generate_uuid()
+            return user.session_id
