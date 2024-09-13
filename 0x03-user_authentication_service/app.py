7ed40d208a81459de84bb8aaf0185d8572732fb8
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-13 20:55:09 +0000

    reset app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -70,7 +70,7 @@ def profile() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=[POST])
+@app.route('/reset_password', methods=['POST'])
 def get_reset_password_token() -> str:
     """_summary_
     """
