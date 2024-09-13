027e60615439716aa998a9663900098ffa69525e
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-13 21:01:38 +0000

    password updated

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -99,3 +99,15 @@ class Auth:
         else:
             user.reset_token = _generate_uuid()
             return user.reset_token
+
+    def update_password(self reset_token: str password: str) -> None:
+        """_summary_
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
