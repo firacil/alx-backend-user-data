72a614da305ed589439f3d83aa7508db7c7ee339
Firaol Mekbib <firaolmekbib511@gmail.com>
2024-09-13 12:14:33 +0000

    change db

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -49,3 +49,16 @@ class DB:
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """use find_user_by to locate the user to
+            update
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+
+        self._session.commit()
+        return None
